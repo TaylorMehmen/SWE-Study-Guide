0081bc0dbcbf3faf33ee47e766d790d341b7ade2
tmehmen2 <tmehmen@wisc.edu>
2022-03-24 08:46:07 -0500

    Quicksort updated

--- Java Algorithms/src/QuickSort/QuickSort.java
+++ Java Algorithms/src/QuickSort/QuickSort.java
@@ -6,9 +6,9 @@ import java.util.Random;
 public class QuickSort {
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] numbers = new int[100];
+        int[] numbers = new int[10];
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i <numbers.length; i++) {
             numbers[i] = rand.nextInt(100);
         }
         System.out.println("Before");
@@ -23,36 +23,31 @@ public class QuickSort {
     private static void quickSort(int[] arr) {
         quickSort(arr, 0, arr.length - 1);
     }
-    private static void quickSort(int[] arr, int lowIndex, int highIndex) {
-        if (lowIndex >= highIndex) return;
-        //choose pivot
-        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+    private static void quickSort(int[] arr, int left, int right) {
+        // return if array is less than 1 length
+        if (left >= right) return;
+        // Select Pivot Randomly to avoid O(n^2) on almost sorted sets
+        int pivotIndex = new Random().nextInt(right - left) + left;
         int pivot = arr[pivotIndex];
-        swap(arr, pivotIndex, highIndex);
-        int lp = partition(arr, lowIndex, highIndex, pivot);
-        quickSort(arr, lowIndex, lp -1);
-        quickSort(arr, lp + 1, highIndex);
-    }
-
-    private static int partition(int[] arr, int lowIndex, int highIndex, int pivot) {
-        int lp = lowIndex;
-        int rp = highIndex;
-        while (lp < rp){
-            while(arr[lp] <= pivot && lp < rp) {
-                lp ++;
-            }
-            while (arr[rp] >= pivot && lp < rp) {
-                rp--;
-            }
-            swap(arr, lp, rp);
+        // Put pivot at the end
+        swap(arr, pivotIndex, right);
+        int lp = left, rp = right-1;
+        while (lp < rp) {
+            while (lp < rp && arr[lp] <= pivot) lp++;
+            while (lp < rp && arr[rp] > pivot) rp--;
+            if (lp < rp) swap(arr, lp, rp);
         }
-        if (arr[lp] > arr[highIndex]) {
-            swap(arr, lp, highIndex);
+        if (arr[rp] > arr[right]) {
+            swap(arr, rp, right);
         }
         else {
-            lp = highIndex;
+            rp = right;
         }
-        return lp;
+        quickSort(arr, left, rp-1);
+        quickSort(arr, rp +1, right);
+
+
+
     }
 
     private static void swap(int[] arr, int index1, int index2) {
